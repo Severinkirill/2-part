37ef114c41a9fadd3b8f8586eb084d6ab11dc189
Severinkirill <severinkirill09@gmail.com>
2025-06-11 12:30:46 +0300

    2 task

---
+++ main.js
@@ -0,0 +1,9 @@
+function separateDigits(number) {
+  const numStr = String(number);
+  return numStr.split('').join(' ');
+}
+
+
+const num = 19878;
+const result = separateDigits(num);
+console.log(result);
